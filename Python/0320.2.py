f75bcb44e941d5710447880634c174926b46e4b3
기강조 <120554102+chogigang@users.noreply.github.com>
2023-03-20 13:38:31 +0900

    Add files via upload

---
+++ Python/0320.2.py
@@ -0,0 +1,6 @@
+a = 1234
+print(a,type(a))
+
+
+b = 12345.6789
+print(b, type(b))
---
+++ Python/2023-03-20 메모.py
@@ -0,0 +1,31 @@
+
+"""
+
+Code lay-out
+
+ 들여쓰기는 공백 4칸을 권장
+
+ teb key (system ,Os ,IDLE)에 따라 다를 수 있음
+ 하나의 프로그램에 두가지 혼용 사용 금지
+
+ 한줄은 최대 79자 까지
+
+
+ 소문자 L ,대문자 O, 대문자 I는 변수명으로 사용금지.
+
+ 클래스 이름은 보편적으로 CapWords convention 사용
+
+
+정수형 &실수형 
+
+a = 1234
+ print(a, type(a))
+
+ b= 12345.6789
+ print(b.type(b))
+
+ type()
+내가 출력하고 있는 값이 int 인지 float인지 string인지 확인
+ 
+ 
+ """
