4827d2c450fa670935b4b3b2e348ef2375d37467
기강조 <120554102+chogigang@users.noreply.github.com>
2023-04-03 11:33:59 +0900

    Add files via upload

---
+++ Python/python(학교수업)/Python/0314.1.py
@@ -0,0 +1,35 @@
+print(2+3)
+print("원투원투")
+print(2+3,"안녕")
+a = 3+3
+print(a)
+print(a+2)
+age = 27
+name = "gigang"
+L_name = "cho"
+print("이름",age)
+print(L_name,name,age)
+print(L_name+name)
+# print(age+name) 같은 타입의 정보만 출력 가능하다.
+print("\"")
+#\ 원래 가지고 있는 뜻을 없앤다는 명령어 ex) 문자열의 출력 ""의  " 일부분을 예외처리
+
+
+
+#  \\ 백슬래시, \
+# \'작은따옴표, Single quote, '
+# \"큰따옴표, Double quote, "
+# \a 벨, ASCII Bell, BEL
+# \b 백스페이스, ASCII Backspace, BS
+# \f 폼피드, ASCII Formfeed, FF
+# \n 새 줄, 개행 문자, ASCII Linefeed, LF
+# \r 캐리지 리턴, ASCII Carriage Return, CR
+# \t 탭 문자, ASCII Horizontal Tab, TAB
+# \v 수직 탭, ASCII Vertical Tab, VT
+# \ooo \ 뒤에 8진수 숫자를 지정하여 ASCII 코드의 문자 표현  예) '\141'은 'a'를 표현
+# \xhh  \ 뒤에 16진수 숫자를 지정하여 ASCII 코드의 문자 표현 예) '\x61'은 'a'를 표현
+# \N {name} { } 안에 문자 이름을 지정하여 유니코드의 문자 표현(파이썬 3.3이상)
+#  \uxxxx  \ 뒤에 16비트 16진수 숫자를 지정하여 유니코드의 문자 표현
+# \Uxxxxxxxx  \ 뒤에 32비트 16진수 숫자를 지정하여 유니코드의 문자 표현
+
+
