4187468e0edbaf5dd6b691fc6c2b0be18d840095
기강조 <120554102+chogigang@users.noreply.github.com>
2023-04-10 15:39:22 +0900

    Add files via upload

--- Python/python(학교수업)/Python/for.py
+++ Python/python(학교수업)/Python/for.py
@@ -18,17 +18,17 @@
  
 
 # sum =0
-# for i in range(1,100):
-#     sum=sum+i   
+# for i in range(1,101):
+#     sum = sum+i   
     
     
     
-# print(sum)
+# print(f"1에서 {i}까지의 합은{sum} 입니다.")
 
 
 # sum = int(input("단수를 입력하세요"))
 
-# for i in range(1,9):      
+# for i in range(1,10):      
 #     print(f"{sum}","*",i,"=",sum*i)
 
 # sum = int(input("단수를 입력하세요"))
@@ -36,8 +36,63 @@
 
 
 
-a= int(input("단수를 입력하세요 :"))
-for i in range(1,10):
-        print(f"{a:2d}* {i:2d} = {a*i:3d}     ",end="")
-        if i%3 ==0 :
-            print("")
+# a= int(input("단수를 입력하세요 :"))
+# for i in range(1,10):
+#         print(f"{a:2d} * {i:2d} = {a*i:3d}     ",end="")
+#         if i%3 ==0 :
+#             print("")
+
+
+# for i in range(1,10):
+#     print("*",end="")
+
+#######################################################################################
+
+
+# for i in range(2,10,1):
+#     print(f"< {i} 단>")
+#     for j in range(1,10,1):
+#             # print(f"{i} * {j} = ",(i*j),end="")
+#             print(f"{i:2d} * {j:2d}  = {i*j:3d} ",end="")
+#             if j%3 == 0:
+#                 print("")
+#     print("")
+
+# num = 0
+# while True :
+#     i= (input("더할 수를 적으시오 0을 적으면 종료"))
+#     if i == '=' :
+
+#         print(f"{num} \n나가임마")
+#         break
+#     else:
+  
+#         num=num+int(i)
+
+       
+#     print(f"{num}")
+
+# while True :
+#     try:
+#         a = int(input("나누기를 할 정수를 입력하세요:"))
+#         b = int(input("나누어질 수를 입력하세요:"))
+#         print(a/b)
+#     except :
+#         print("에외가 발생")
+    
+num = 0
+while True:
+    try: 
+        i = (input("더할 수를 적으시오 =를 적으면 종료"))
+        if i == '=' :
+            print(f"{num}\n종료") 
+            break
+        else:
+            num=num+int(i)
+    except: 
+        print("잘못 입력") 
+        
+        
+    
+
+
