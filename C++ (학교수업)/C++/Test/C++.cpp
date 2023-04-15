c65f9eeb8f0ba6d7e54f7a3e7fb11597f0889581
기강조 <120554102+chogigang@users.noreply.github.com>
2023-04-16 02:40:18 +0900

    과제 문제
    과제 문제

---
+++ C++ (학교수업)/C++/Test/C++.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+using namespace std;
+/*
+void main() {
+
+	int num;
+
+	cout << "값을 입력하세요" << endl;
+	cin >> num;
+
+	string a = "짝수";
+	string b = "홀수";
+	
+	string result = (num % 2 == 0) ? a : b;
+
+	cout << num << "는" << result << endl;
+
+
+}
+
+*/
+
+/*
+
+int main() {
+	
+	int num;
+	
+	cout << "10000 이하의 값을 입력하세요: ";
+	
+	cin >> num;
+	
+	string a = (num >= 1 && num <= 10000) ? ((num >= 1000) ? "4" :
+		
+		(num >= 100) ? "3" :
+		
+		(num >= 10) ? "2" : "1") : "1만초과";
+	
+	cout << a << endl;
+
+	return 0;
+}
+*/
+
+//void main() {
+//	
+//	cout << double(1) / 2 << endl;
+//}
+
