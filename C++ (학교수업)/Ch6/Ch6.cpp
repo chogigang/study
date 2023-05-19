87f921777225bbf94114bb8185828fa96108e508
기강조 <120554102+chogigang@users.noreply.github.com>
2023-05-20 00:04:16 +0900

    Add files via upload

--- C++ (학교수업)/Ch6/Ch6.cpp
+++ C++ (학교수업)/Ch6/Ch6.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 //int main()
@@ -223,19 +222,176 @@ using namespace std;
 //3번 
 
 
+//void main() {
+//	int arr[] = { 1,6,9,7,3,2,0,4,8,5 };
+//	int min = 0;
+//	int max = 0;
+//	const int N = size(arr); //원소 갯수
+//
+//
+//	int b[N] = {};
+//	for (int i = 0; i < N; i++) {
+//		b[i] = arr[(N - 1) - i];//3번
+//
+//	
+//	}
+//	
+//}
+
+
+//4번 
+//void main() {
+//    int arr[] = { 1, 6, 9, 7, 3, 2, 0, 4, 8, 5 };
+//    int temp;
+//    int n = size(arr);
+//
+//    for (int i = 0; i < n - 1; i++) {
+//        for (int j = i + 1; j < n; j++) {
+//            if (arr[i] > arr[j]) { //오름차순
+//                temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//    }
+//
+//    for (int k = 0; k < n; k++) {
+//        cout << arr[k] << ",";
+//    }
+//
+//    
+//}
+
+
+//void main() {
+//    int arr[] = { 1, 6, 9, 7, 3, 2, 0, 4, 8, 5 };
+//    int temp;
+//    int n = size(arr);
+//
+//    for (int i = 0; i < n - 1; i++) {
+//        for (int j = i + 1; j < n; j++) {
+//            if (arr[i] < arr[j]) { //내림차순
+//                temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//    }
+//
+//    for (int k = 0; k < n; k++) {
+//        cout << arr[k] << ",";
+//    }
+//
+//
+//}
+
+
+
+//call by value
+
+// 함수는 무조건 main 위에 무조건 있어야한다.
+// 
+// 
+//void func(int arg) { //arg 매개변수 함수의 전달값을 받는 변수
+//
+//
+//	arg++; 
+//	cout << arg << endl; //2
+//
+//}
+//
+//
+//void main() {
+//	int a = 1;
+//	func(a); //함수 호출 전달값은 1 
+//	cout << a << endl; //1
+//
+//}
+// 
+// 
+// 변수로 함수를 넘길때는 값이 넘어가지만 배열로 함수로 넘기면 주소가 넘겨진다
+// 이 둘은 차이가 있다 C++는 배열의 값을 대입연산자로 넣을수가 없다.
+
+
+// call by reference
+
+//void func(int arg[1]) {
+//	arg[0]++; //0번째 인덱스 값을 +1을 시킴
+//	cout << arg[0] << endl; //2
+//
+//}
+//
+//void main() {
+//	int arr[1] = { 1 };
+//	func(arr);
+//	cout << arr[0] << endl; //2
+//
+//}
+
+//서로 공유를 하고 있기 때문에 함수에서 값을 추가시키면서 main 함수에 있는 값도 증가함
+
+
+//void func(int arg[4]) {
+//	cout << sizeof(arg) << endl;  //4or 8
+//	cout << arg[0] << endl;
+//	cout << arg[1] << endl;
+//}
+//
+//
+//void main() {
+//
+//	int arr[4] = { 1,2,3,4 };
+//	func(arr);
+//
+//}
+
+
+//void func(int arg[2][3]) {
+//
+//	arg[1][2] = 1;
+//
+//}
+//
+//void main() {
+//	int arr[2][3] = { {0} };
+//	func(arr);
+//	cout << arr[1][2] << endl;
+//
+//}
+//
+
+//void main() {
+//
+//
+//	int arr[2][3];
+//	cout << sizeof(arr) << endl; //배열의 메모리 크기
+//	cout << sizeof(arr[0]) << endl;
+//	cout << sizeof(arr[0][0]) << endl;
+//
+//
+//}
+
+
+ // 배열을 이용하여 앙수의 합과 음수의 합을 구하는 프로그램을 작성하시오.
 void main() {
-	int arr[] = { 1,6,9,7,3,2,0,4,8,5 };
-	int min = 0;
-	int max = 0;
-	const int N = size(arr); //원소 갯수
+    int a = 0; //양수의 합 변수
+    int b = 0; //음수의 합 변수
+   
+    int arr[] = { 15,-6,3,8,-2,5,6,-10,21,13,16,-4,-9,-14,7 };
+    int n = size(arr);
+    for (int i = 0; i< n; i++) {
+        if (arr[i] > 0) {
+            a += arr[i];
+        }
+        else {
+            b += arr[i];
+        }
+    }
 
 
-	int b[N] = {};
-	for (int i = 0; i < N; i++) {
-		b[i] = arr[(N - 1) - i];//3번
+    cout << "양수 합 :" << a << endl;
+    cout << "음수 합: " << b << endl;
 
-	
-	}
-	
-	
-	}
+}
+
+
