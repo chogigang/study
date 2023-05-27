ddb4444c847507fcaececfd9fcb71716515170c1
기강조 <120554102+chogigang@users.noreply.github.com>
2023-05-28 00:10:11 +0900

    Add files via upload

---
+++ C++ (학교수업)/Ch8/8Ch.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+//int Multiply(int a, int b) {
+//	int Result = a * b;
+//	return Result;
+//	Result = 0;
+//
+//}
+//
+//
+//void main(){
+//
+//
+//	int Result = Multiply(3, 4);
+//	cout << Result << endl; //12출력
+//	return;
+//
+//}
+
+
+//void main() {
+//
+//	double Result = Multiply(3.3, 7.7);
+//	cout << Result << endl; //Error
+//
+//}
+//
+//double Multiply(double a, double b) {
+//
+//	return a * b;
+//
+//}
+
+
+//double Multiply(double a, double b) {
+//
+//	return a * b;
+//
+//}
+//
+//void main() {
+//
+//	double Result = Multiply(3.3, 7.7);
+//	cout << Result << endl;
+//
+//}
+
+
+//void Sub(int arg, int m) {
+//
+//	Mod(arg - m, m); //Error
+//}
+//
+//
+//void Mod(int arg,int m) {
+//	if (arg < m) { 
+//		cout << arg << endl;
+//	}
+//	else {
+//		Sub(arg, m); //Error 이걸 위로 올려도 Sub단에서 에러가 걸릴것이다.
+//	
+//	}
+//
+//}
+//
+//
+//void main() {
+//	Mod(11, 3);
+//
+//}
+
+
+//void Mod(int, int);
+//
+//void Sub(int arg, int m) {
+//
+//	Mod(arg - m, m); //OK
+//}
+//
+//
+//void Mod(int arg,int m) {
+//	if (arg < m) { 
+//		cout << arg << endl;
+//	}
+//	else {
+//		Sub(arg, m);
+//	
+//	}
+//
+//}
+//
+//
+//void main() {
+//	Mod(11, 3);
+//
+//}
+//
+
+
+#include <stdio.h>
+void main() {
+	printf("C++\r\n");
+}
